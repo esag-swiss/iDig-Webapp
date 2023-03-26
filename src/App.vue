457890e6b30d219378a0df3dca506e261f37e922
francois <francoiscrouzet@posteo.de>
2023-03-26 16:18:25 +0200

    Move file from original project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,129 @@
-<script >
-</script>
-
 <template>
-  <div></div>
+  <!-- header -->
+  <header-idig @toggle-menu="toggleMenu" @all-trenches="setallTrench" @all-types="setallTypes" >
+  </header-idig>
+
+  <div class="container-fluid">
+    <div class="row flex-xl-nowrap">
+      <!-- SIDEBAR -->
+      <div class="p-1 col-md-2" v-bind:style="{ display: computedDisplay }">
+        <div class="sticky-top">
+          <access-idig
+            @selected-trench="selectedTrench"
+
+            :all-trenches="allTrench"
+          >
+          </access-idig>
+
+          <filter-fields
+            @check-fields="checkFields"
+            @selected-type="selectedType"
+            :selected-data="selectedData"
+          >
+          </filter-fields>
+        </div>
+      </div>
+
+      <!-- MAIN FRAME The map or tab goes here -->
+      <div
+        class="p-1"
+        :class="{ 'col-md-10': isHidden, 'col-md-12': !isHidden }"
+      >
+        <dyna-table
+          :selected-data="selectedData"
+          :checked-fields="checkedFields"
+
+          :selected-type="selectedFilter"
+          :all-types="allTypes"
+        >
+        </dyna-table>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import AccessIdig from "./components/AccessIdig.vue";
+import HeaderIdig from "./components/HeaderIdig.vue";
+import FilterFields from "./components/FilterFields.vue";
+import DynaTable from "./components/DynaTable.vue";
+import preferencesData from "./data/Preferences.json";
+import Data from "./data/AMA21-S24.json"; //Default data
 
+export default {
+  name: "App",
+  components: {
+    AccessIdig,
+    FilterFields,
+    DynaTable,
+    HeaderIdig,
+  },
+  data() {
+    return {
+      isHidden: true,
+      display: "block",
+      class: true,
+      trenchData: Data, // load default data, will be populate when selecting trenches on AccessIdig componant
+      fields: preferencesData.types,
+      selectedFilter: "Artifact", // type by default
+      checkedFields: [
+        // columns by default before any selection /!\ label needed to display headers
+        { field: "Source", sortable: true, label: "Secteur" },
+        { field: "Title", sortable: true, label: "Titre" },
+        {
+          field: "Identifier",
+          isKey: true,
+          sortable: true,
+          label: "Identifiant",
+        },
+      ],
+      // trenchesversion: {},
+      allTrench: [],
+      allTypes: [],
+    };
+  },
+  computed: {
+    selectedData() {
+      return this.trenchData.filter((object) => {
+        return object.Type.includes(this.selectedFilter);
+      });
+    },
+    computedDisplay() {
+      return this.display;
+    },
+  },
+  methods: {
+    toggleMenu() {
+      //a simplifier avec :class
+      if (this.display == "none") {
+        this.display = "block";
+      } else {
+        this.display = "none";
+      }
+      this.isHidden = !this.isHidden;
+    },
+    // reçoit des enfants
+    setallTrench(allTrenches) { // recoit toutes les trenches de HeaderIdig
+      this.allTrench = allTrenches;
+    },
+    setallTypes(types) {
+      this.allTypes = types;
+    },
+    selectedTrench(trench) {
+      this.trenchData = trench;
+    },
+    selectedType(type) {
+      this.selectedFilter = type;
+    },
+    checkFields(emited) {
+      this.checkedFields = emited;
+    },
+  },
+};
+</script>
+
+<style>
+#app h3 {
+  font-size: 1.3rem;
+}
 </style>
---
+++ src/components/TableSimple.vue
@@ -0,0 +1,47 @@
+<template>
+  <thead>
+    <tr>
+      <th v-for="group in filteredField" :key="group">{{group.field}}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="row in filtered" :key="row.id">
+      <td v-for="group in filteredField" :key="group">{{row[group.field]}}</td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+import trenchData from "../data/AMA21-S24.json";
+import preferencesData from "../data/AMA21-S24.Preferences.json";
+
+export default {
+  data() {
+    return {
+      trenchdata: trenchData,
+      // headers: preferencesData.fields,
+      fields: preferencesData.types,
+    };
+  },
+  props: {
+    selectedFilter: {
+      type: String,
+      required: false
+    },   
+  },
+  computed: {
+    filteredField(){
+     return this.fields.filter(x => {
+      return x.type.includes(this.selectedFilter)
+     })[0].groups[0].fields
+    },
+    filtered(){
+     return this.trenchdata.filter(object => {
+        return object.Type.includes(this.selectedFilter)
+     })
+    }
+    
+  },
+
+};
+</script>
