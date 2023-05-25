ba0da9d9d67cfd53da29c08cb0ce26ed449dc76a
Jean Sébastien Gros <106298905+AlZorba@users.noreply.github.com>
2023-05-25 06:27:19 +0200

    Use Qasar for TheControlExport (#62)
    **Issue** :  `https://github.com/esag-swiss/iDig-Webapp/issues/16`
    
    **Description** :    
    This PR introduce the use of Quasar for the export button.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import { createApp } from "vue";
 import { Quasar } from "quasar";
 
 // Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
 import "@quasar/extras/material-icons/material-icons.css";
-
+import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
+import "@quasar/extras/material-icons-round/material-icons-round.css";
+import "@quasar/extras/material-icons-sharp/material-icons-sharp.css";
+import "@quasar/extras/material-symbols-outlined/material-symbols-outlined.css";
+import "@quasar/extras/material-symbols-rounded/material-symbols-rounded.css";
+import "@quasar/extras/material-symbols-sharp/material-symbols-sharp.css";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
+import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+// Import Quasar css
+import "quasar/src/css/index.sass";
 import App from "./App.vue";
 
 import "./assets/main.css";
