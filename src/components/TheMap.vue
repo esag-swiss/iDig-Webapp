780b2e31587ce2e7921b6307adb19b79fd7f988b
AlZorba <jsgros3@gmail.com>
2023-11-17 15:22:32 +0100

    Change tile server

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -37,12 +37,12 @@ export default {
     });
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}",
+      "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
       {
         maxZoom: 25,
         maxNativeZoom: 19,
         attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         ext: "png",
       }
     ).addTo(this.map);
