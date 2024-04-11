2b6d72ca888d3a0423ed52befff1decae1647c41
AlZorba <jsgros3@gmail.com>
2024-04-11 10:04:37 +0200

    add  v-slot:body

--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -34,8 +34,15 @@
             >
           </q-th>
         </q-tr>
-      </template></q-table
-    >
+      </template>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
