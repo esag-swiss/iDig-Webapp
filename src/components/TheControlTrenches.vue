f6052d6350565bca6c1053d53ed6502f9527b06f
AlZorba <jsgros3@gmail.com>
2024-09-29 11:42:04 +0200

    No accordion < 10

--- src/components/TheControlTrenches.vue
+++ src/components/TheControlTrenches.vue
@@ -1,58 +1,77 @@
 <template>
   <div name="secteurs" class="p-1 m-1 bg-light border-0">
     <h3>Secteurs</h3>
-
-    <ul
-      v-for="(trenchGroupName, index) in accordionLabels"
-      :key="trenchGroupName"
-      class="list-group"
-    >
-      <li
-        class="list-group-item accordion text-bold"
-        @click="isDisplayedArray[index] = !isDisplayedArray[index]"
+    <!-- liste trenches -->
+    <div v-if="projectTrenchesNames.length < 10">
+      <ul
+        v-for="trenchName in projectTrenchesNames"
+        :key="trenchName"
+        class="list-group"
       >
-        <label class="px-1 m-0 text-bold" for="checkbox">
-          <q-icon
-            v-if="isDisplayedArray[index]"
-            name="keyboard_arrow_down"
-          /><q-icon
-            v-if="!isDisplayedArray[index]"
-            name="keyboard_arrow_right"
-          />{{ trenchGroupName }}</label
+        <li class="list-group-item accordion">
+          <input
+            id="trench-checkbox"
+            v-model="checkedTrenchesNames"
+            type="checkbox"
+            :value="trenchName"
+          />
+          <label class="px-1 m-0" for="trench-checkbox">{{ trenchName }}</label>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <ul
+        v-for="(trenchGroupName, index) in accordionLabels"
+        :key="trenchGroupName"
+        class="list-group"
+      >
+        <li
+          class="list-group-item accordion text-bold"
+          @click="isDisplayedArray[index] = !isDisplayedArray[index]"
         >
-        <input
-          v-model="isCheckedArray[index]"
-          type="checkbox"
-          @change="checkGroup(trenchGroupName, isCheckedArray[index])"
-        />
-      </li>
-      <!-- liste trenches -->
-      <div v-if="isDisplayedArray[index]">
-        <ul
-          v-for="trenchName in projectTrenchesNames"
-          :key="trenchName"
-          class="list-group"
-        >
-          <li
-            v-if="trenchName.includes(trenchGroupName)"
-            class="list-group-item accordion"
+          <label class="px-1 m-0 text-bold" for="checkbox">
+            <q-icon
+              v-if="isDisplayedArray[index]"
+              name="keyboard_arrow_down"
+            /><q-icon
+              v-if="!isDisplayedArray[index]"
+              name="keyboard_arrow_right"
+            />{{ trenchGroupName }}</label
           >
-            <input
-              id="trench-checkbox"
-              v-model="checkedTrenchesNames"
-              type="checkbox"
-              :value="trenchName"
-            />
-            <label class="px-1 m-0 text-bold" for="trench-checkbox">{{
-              trenchName
-            }}</label>
-          </li>
-        </ul>
-      </div>
-    </ul>
+          <input
+            v-model="isCheckedArray[index]"
+            type="checkbox"
+            @change="checkGroup(trenchGroupName, isCheckedArray[index])"
+          />
+        </li>
+        <!-- liste trenches -->
+        <div v-if="isDisplayedArray[index]">
+          <ul
+            v-for="trenchName in projectTrenchesNames"
+            :key="trenchName"
+            class="list-group"
+          >
+            <li
+              v-if="trenchName.includes(trenchGroupName)"
+              class="list-group-item accordion"
+            >
+              <input
+                id="trench-checkbox"
+                v-model="checkedTrenchesNames"
+                type="checkbox"
+                :value="trenchName"
+              />
+              <label class="px-1 m-0" for="trench-checkbox">{{
+                trenchName
+              }}</label>
+            </li>
+          </ul>
+        </div>
+      </ul>
+    </div>
 
     <!-- Check All/None -->
-    <div class="q-pa-xs">
+    <div v-if="projectTrenchesNames.length > 10" class="q-pa-xs">
       <div class="q-gutter-sm">
         <q-btn
           align="left"
