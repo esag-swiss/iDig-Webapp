f1fc5d0e8f0fd047a70954e3b2f84501388d2d41
AlZorba <jsgros3@gmail.com>
2025-02-21 10:08:17 +0100

    Introduce Tabulator

--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -9,7 +9,8 @@
     @clearTheItem="clearTheItem, setSelectedItem(null)"
   ></ThePatches>
   <TheItem v-if="selectedItem"> </TheItem>
-  <div class="q-pa-xs">
+
+  <div v-if="false" class="q-pa-xs">
     <q-table
       v-model:pagination="pagination"
       row-key="name"
@@ -59,11 +60,18 @@
       </template>
     </q-table>
   </div>
+  <div ref="table" style="width: 100%; height: 205px"></div>
 </template>
 
 <script>
 import { ref, watch } from "vue";
+import "tabulator-tables/dist/css/tabulator_simple.min.css";
+
 import { mapActions, mapState } from "pinia";
+import {
+  TabulatorFull as Tabulator,
+  MoveColumnsModule,
+} from "tabulator-tables";
 import { useDataStore } from "@/stores/data";
 import { useAppStore } from "@/stores/app";
 import TheItem from "@/components/TheItem.vue";
@@ -72,6 +80,7 @@ import ThePatches from "@/components/ThePatches.vue";
 export default {
   name: "TheQTable",
   components: { TheItem, ThePatches },
+
   setup() {
     const dataStore = useDataStore();
     const appStore = useAppStore();
@@ -217,15 +226,116 @@ export default {
       onDragOver,
     };
   },
+  data() {
+    return {
+      tabulator: null, //variable to hold your table
+    };
+  },
+
   computed: {
     ...mapState(useDataStore, [
       "syncPatches",
       "setCheckedFieldNames",
       "selectedItem",
+      "checkedFieldNames",
+      "projectPreferencesFieldsWithTranslation",
     ]),
+
+    columnsTabulator() {
+      var headerMenu = [
+        {
+          label: "Hide Column",
+          action: (e, column) => {
+            console.log(" Column name", column.getField());
+            this.setCheckedFieldNames(
+              this.checkedFieldNames.filter(
+                (fieldName) => fieldName !== column.getField()
+              )
+            );
+          },
+        },
+      ];
+      return this.checkedFieldNames.map((fieldName) => ({
+        title: this.projectPreferencesFieldsWithTranslation[fieldName],
+        headerFilter: "input",
+        field: fieldName,
+        headerMenu: headerMenu,
+      }));
+    },
+  },
+
+  mounted() {
+    //instantiate Tabulator when element is mounted
+    this.tabulator = new Tabulator(this.$refs.table, {
+      data: this.rows, //link data to table
+      reactiveData: true, //turn on data reactivity
+      layout: "fitColumns", //fit columns to width of table (optional)
+      movableColumns: true,
+      columns: this.columnsTabulator, //define table columns
+      minHeight: "100%",
+      rowContextMenu: [
+        {
+          label: "Copy to clipboard",
+
+          action: function (e, row) {
+            const rowData = row.getData();
+            const text = JSON.stringify(rowData, null, 2);
+            navigator.clipboard
+              .writeText(text)
+              .catch((err) => console.error("Failed to copy: ", err));
+          },
+        },
+        {
+          separator: true,
+        },
+        {
+          disabled: false,
+          label: "Open to new tab",
+          action: (e, row) => {
+            this.openInNewTab2(row.getData());
+          },
+        },
+      ],
+    });
+
+    this.$watch(
+      () => this.rows,
+      (newRows) => {
+        if (this.tabulator) {
+          this.tabulator.replaceData(newRows);
+        }
+      },
+      { deep: true }
+    );
+
+    this.$watch(
+      () => this.columnsTabulator,
+      (newCols) => {
+        if (this.tabulator) {
+          this.tabulator.setColumns(newCols);
+        }
+      },
+      { deep: true }
+    );
+
+    this.tabulator.on("rowClick", (e, row) => {
+      this.setSelectedItem(row.getData());
+      this.setIsItemSelected(true);
+    });
   },
   methods: {
     ...mapActions(useDataStore, ["setSyncPatches", "setSelectedItem"]),
+    ...mapActions(useAppStore, ["setIsItemSelected"]),
+
+    openInNewTab2(row) {
+      const link = this.$router.resolve({
+        name: "TheItemStandalone",
+        params: { itemId: row.IdentifierUUID, trenchSource: row.Trench },
+      }).href;
+
+      window.open(link, "_blank");
+    },
+
     openInNewTab(eve, row) {
       // Prevent the default context menu from opening
       eve.preventDefault();
