da4ce553974992c5b243a3b521a8a0bb861c5f0c
AlZorba <jsgros3@gmail.com>
2025-06-22 20:48:39 +0200

    Add i18n support and update App.vue for multilingual functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import { Quasar, Notify } from "quasar";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+import fr from "./locales/fr.json";
+import en from "./locales/en.json";
+import de from "./locales/de.json";
+import it from "./locales/it.json";
+import el from "./locales/el.json";
 
 // Import icon libraries
 import "@quasar/extras/roboto-font/roboto-font.css";
@@ -21,9 +27,21 @@ import "./assets/main.css";
 import router from "./router/index.js"; // Assurez-vous que le routeur est importé
 
 const pinia = createPinia();
+const i18n = createI18n({
+  locale: "fr", // langue par défaut
+  fallbackLocale: "en", // langue de secours
+  messages: {
+    fr,
+    en,
+    de,
+    it,
+    el,
+  },
+});
 const myApp = createApp(App);
 myApp.use(router);
 myApp.use(pinia);
+myApp.use(i18n);
 myApp.use(Quasar, {
   plugins: {
     Notify,
